b63a53a870eb609dc9d610c8c6cf31dc4280fca0
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-05-31 15:39:43 +0200

    added mail exercise (no bonus)

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Esercizio mail
+# Esercizio mail ( senza .includes( ) )
 
 - Creo un'array di email
 - Prendo l'elemento dal DOM
 - Chiedo all'utente una mail e la salvo
+    - Valido i dati inseriti dall'utente
+- Verifico se la mail salvata è presente nell array:
+    - **SE** è presente nell'array:
+        - Do il benevenuto
+    - **ALTRIMENTI**:
+        - Dico che non puo accedere
--- js/mail.js
+++ js/mail.js
@@ -1,1 +1,21 @@
-console.log('JS OK');
+console.log('JS mail OK');
+
+const emailsList = ['matteo@boolean.com', 'luca@boolean.com', 'marco@boolean.com', 'mario@boolean.com', 'gino@boolean.com', 'maria@boolean.com'];
+console.table(emailsList);
+const emailTarget = document.getElementById('emailPlacer');
+const userEmail = prompt('Inserisci la tua e-mail', 'gino@boolean.com').trim();
+let isValid = false;
+
+for (let i = 0; i < emailsList.length; i++){
+    if(userEmail === emailsList[i]){
+        isValid = true;
+    }
+};
+
+console.log(isValid);
+
+if(isValid){
+    console.log('Acesso effettuato correttamente, benvenuto!');
+} else {
+    console.log('Acesso fallito, email non valida!');
+};
