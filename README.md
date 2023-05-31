957e928403f78ecf93b2b6c4609ec95f47801c12
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-05-31 17:07:19 +0200

    added second exercise

--- README.md
+++ README.md
@@ -4,8 +4,23 @@
 - Prendo l'elemento dal DOM
 - Chiedo all'utente una mail e la salvo
     - Valido i dati inseriti dall'utente
-- Verifico se la mail salvata è presente nell array:
+- Verifico se la mail salvata è presente nell array con un ciclo senza usare .includes( ):
     - **SE** è presente nell'array:
         - Do il benevenuto
     - **ALTRIMENTI**:
         - Dico che non puo accedere
+
+
+
+# Esercizio dadi
+
+- Genero un numero per l'utente e lo salvo
+- Genero un numero per il computer e lo salvo
+- **SE** il numero dell'utente è maggiore di quello del computer:
+    - L'utente ha vinto
+- **ALTRIMENTI SE** i numeri sono uguali:
+    - Pareggio
+- **ALTRIMENTI**:
+  - Il computer ha vinto
+
+
--- js/dadi.js
+++ js/dadi.js
@@ -1,1 +1,21 @@
 console.log('JS dadi OK');
+
+const dadiPlacer = document.getElementById('dadiPlacer');
+const pcNumber = Math.floor(Math.random() * 6)  +  1;
+console.log('Numero del computer: ' +  pcNumber);
+const userNumber = Math.floor(Math.random() * 6)  +  1;
+console.log('Numero dell\'user: ' +  userNumber);
+let result = `<h4>Il tuo numero: ${userNumber} - numero di Marco: ${pcNumber}</h4>`;
+
+if(userNumber > pcNumber){
+    console.log('L\'utente ha vinto!');
+    result += '<h4>HAI BATTUTO MARCO!</h4>'
+} else if(userNumber === pcNumber){
+    console.log('I numeri sono ugali: pareggio!');
+    result += '<h4>PAREGGIO!</h4>'
+} else {
+    console.log('Il computer ha vinto!');
+    result += '<h4> MARCO TI HA SCONFITTO!</h4>'
+}
+
+dadiPlacer.innerHTML = result;
